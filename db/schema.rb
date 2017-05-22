906e19496bd7e7b3ea03a2c6bb549cc8fb4da84e
clxhope <changluxin@gmail.com>
2017-05-22 08:25:04 +0800

    收货人增加电话信息

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304064756) do
+ActiveRecord::Schema.define(version: 20170522000849) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170304064756) do
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
+    t.string   "phone"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
@@ -58,6 +59,11 @@ ActiveRecord::Schema.define(version: 20170304064756) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
