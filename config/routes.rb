42dad8accba1df96096018613583104de5172def
mww0595 <mww0595@gmail.com>
2017-06-11 13:34:53 +0800

    增加赛事页面

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index'
 get 'about' => 'welcome#about'
+get 'racing' => 'welcome#racing'
 
 resources :products do
   member do
